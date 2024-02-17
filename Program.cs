6ee994fbe517286a7df895099f664e84597dd24b
dilshadbrhm <dilshadvi@code.edu.az>
2024-02-17 12:10:21 +0400

    first commit

--- Program.cs
+++ Program.cs
@@ -5,152 +5,155 @@ namespace task3
     internal class Program
     {
         static void Main(string[] args)
-        {   ////1
-            //Console.Write("Enter number : ");
-            //string number = Console.ReadLine();
-            //string sum = "7" + number + "8";
-            //Console.WriteLine(sum);
+        {
 
-            ////2
-            //Console.Write("Enter number : ");
-            //int number;
-            //number = Convert.ToInt32(Console.ReadLine());
-            //int sum = number * 1000 + number;
-            //Console.WriteLine(sum);
 
-            ////3
-            //Console.Write("Enter number : ");
-            //float number;
-            //number = Convert.ToInt32(Console.ReadLine());
-            //float result = (number * 18) / 100;
-            //float result1 = (number * 3) / 100;
-            //Console.WriteLine(result);
-            //Console.WriteLine(result1);
+            //{   ////1
+            //    Console.Write("Enter number : ");
+            //    int number = Convert.ToInt32(Console.ReadLine());
+            //    int result = 70000 + ((number * 10) + 8);
 
-            ////4
-            //Console.Write("Enter number :");
-            //int number;
-            //number = Convert.ToInt32(Console.ReadLine());
-            //float  result = (number * 10) + 7;
-            //float result2 = result * 7 / 100;
-            //Console.WriteLine(result2);
 
-            ////5
-            //Console.Write("Enter number : ");
-            //int number;
-            //number = Convert.ToInt32(Console.ReadLine());
-            //float result = (number + 40000);
-            //float result2 = result * 100 + 44;
-            //Console.WriteLine(result2);
-            //float result3 = (result2 * 44) / 100;
-            //Console.WriteLine(result3);
-
-            ////6
-            //Console.Write("Enter number : ");
-            //int number;
-            //number = Convert.ToInt32(Console.ReadLine());
-            //float result = number * 20 / 100 ;
-            //float result2 = result * 10 / 100;
-            //Console.WriteLine(result2 * result2);
-
-            //7
-            //Console.Write("Enter number1 :");
-            //int number1;
-            //number1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number2 :");
-            //int number2;
-            //number2 = Convert.ToInt32(Console.ReadLine());
-            //int result = number1 + number2;
-            //if (result < 100000)
-            //{
-            //    float result2 = (result + 500000) * 10 + 5;
-            //    float result3 = (result2 * 5) / 100;
-            //    Console.WriteLine(result3);
-            //}
-            //else
-            //{
-            //    double sum1 = (result + 5000000) * 10 + 5;
-            //    double sum2 = (sum1 * 5) / 100;
-            //    Console.WriteLine(sum2);
-            //}
-
-            ////8
-            //Console.Write("Enter number1 :");
-            //int number1;
-            //number1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number2 :");
-            //int number2;
-            //number2 = Convert.ToInt32(Console.ReadLine());
-            //double result1 = number1 * 4 / 100;
-            //double result2 = number2 * 9 / 100;
-            //double result3 = result1 + result2;
-            //if (result3 < 1000000)
-            //{
-            //    double sum1 = result3 * 10 / 100;
-            //    Console.WriteLine(sum1);
-
-            //}
-            //else
-            //{
-            //    double sum2= result3 * 10 / 100;
-            //    Console.WriteLine(sum2);    
-            //}
-
-            ////9
-            //Console.Write("Enter number1 :");
-            //int number1;
-            //number1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number2 :");
-            //int number2;
-            //number2 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number3 :");
-            //int number3;
-            //number3 = Convert.ToInt32(Console.ReadLine());
-            //float result1 = number1 * 10 / 100;
-            //float result2 = number2 * 10 / 100;
-            //float result3 = number3 * 10 / 100;
-            //float last = result1 + result2 + result3;
-            //if (last <100000)
-            //{
-            //    float sum = last / 10;
+            //    //2
+            //    Console.Write("Enter number : ");
+            //    int number;
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    int sum = number * 1000 + number;
             //    Console.WriteLine(sum);
-            //}
-            //else
-            //{
-            //    float sum1 = last / 10;
-            //    Console.WriteLine(sum1);
-            //}
 
-            ////10
-            //Console.Write("Enter number1 :");
-            //int number1;
-            //number1 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number2 :");
-            //int number2;
-            //number2 = Convert.ToInt32(Console.ReadLine());
-            //Console.Write("Enter number3 :");
-            //int number3;
-            //number3 = Convert.ToInt32(Console.ReadLine());
-            //float result1 = number1 * 1 / 100;
-            //float result2 = number2 * 2 / 100;
-            //float result3 = number3 * 3 / 100;
-           
-            //float result4 = result3 - result2 - result1;
-            //float result5 = number3 * 7 / 100;
-            //float result6 = result4 + result5;
-            //Console.WriteLine(result6);
-            
-            
+            //    //3
+            //    Console.Write("Enter number : ");
+            //    float number;
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    float result = (number * 18) / 100;
+            //    float result1 = (number * 3) / 100;
+            //    Console.WriteLine(result);
+            //    Console.WriteLine(result1);
 
-           
+            //    //4
+            //    Console.Write("Enter number :");
+            //    int number;
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    float result = (number * 10) + 7;
+            //    float result2 = result * 7 / 100;
+            //    Console.WriteLine(result2);
 
+            //    //5
+            //    Console.Write("Enter number : ");
+            //    int number;
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    float result = (number + 40000);
+            //    float result2 = result * 100 + 44;
+            //    Console.WriteLine(result2);
+            //    float result3 = (result2 * 44) / 100;
+            //    Console.WriteLine(result3);
 
-            
+            //    //6
+            //    Console.Write("Enter number : ");
+            //    int number;
+            //    number = Convert.ToInt32(Console.ReadLine());
+            //    float result = number * 20 / 100;
+            //    float result2 = result * 10 / 100;
+            //    Console.WriteLine(result2 * result2);
+
+            //    7
+            //    Console.Write("Enter number1 :");
+            //    int number1;
+            //    number1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number2 :");
+            //    int number2;
+            //    number2 = Convert.ToInt32(Console.ReadLine());
+            //    int result = number1 + number2;
+            //    if (result < 100000)
+            //    {
+            //        float result2 = (result + 500000) * 10 + 5;
+            //        float result3 = (result2 * 5) / 100;
+            //        Console.WriteLine(result3);
+            //    }
+            //    else
+            //    {
+            //        double sum1 = (result + 5000000) * 10 + 5;
+            //        double sum2 = (sum1 * 5) / 100;
+            //        Console.WriteLine(sum2);
+            //    }
+
+            //    //8
+            //    Console.Write("Enter number1 :");
+            //    int number1;
+            //    number1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number2 :");
+            //    int number2;
+            //    number2 = Convert.ToInt32(Console.ReadLine());
+            //    double result1 = number1 * 4 / 100;
+            //    double result2 = number2 * 9 / 100;
+            //    double result3 = result1 + result2;
+            //    if (result3 < 1000000)
+            //    {
+            //        double sum1 = result3 * 10 / 100;
+            //        Console.WriteLine(sum1);
+
+            //    }
+            //    else
+            //    {
+            //        double sum2 = result3 * 10 / 100;
+            //        Console.WriteLine(sum2);
+            //    }
+
+            //    //9
+            //    Console.Write("Enter number1 :");
+            //    int number1;
+            //    number1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number2 :");
+            //    int number2;
+            //    number2 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number3 :");
+            //    int number3;
+            //    number3 = Convert.ToInt32(Console.ReadLine());
+            //    float result1 = number1 * 10 / 100;
+            //    float result2 = number2 * 10 / 100;
+            //    float result3 = number3 * 10 / 100;
+            //    float last = result1 + result2 + result3;
+            //    if (last < 100000)
+            //    {
+            //        float sum = last / 10;
+            //        Console.WriteLine(sum);
+            //    }
+            //    else
+            //    {
+            //        float sum1 = last / 10;
+            //        Console.WriteLine(sum1);
+            //    }
+
+            //    //10
+            //    Console.Write("Enter number1 :");
+            //    int number1;
+            //    number1 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number2 :");
+            //    int number2;
+            //    number2 = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("Enter number3 :");
+            //    int number3;
+            //    number3 = Convert.ToInt32(Console.ReadLine());
+            //    float result1 = number1 * 1 / 100;
+            //    float result2 = number2 * 2 / 100;
+            //    float result3 = number3 * 3 / 100;
+
+            //    float result4 = result3 - result2 - result1;
+            //    float result5 = number3 * 7 / 100;
+            //    float result6 = result4 + result5;
+            //    Console.WriteLine(result6);
+
 
 
 
 
 
         }
+
+
+             
+
+
+        
     }
 }
